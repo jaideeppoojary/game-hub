bf20ad7973159642c0e224f3c0c854f193ed7173
Jaideep <jaideep.jaidee@gmail.com>
2023-07-04 21:37:45 +0530

    Respinsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 // import './App.css'
-import { Button } from '@chakra-ui/react';
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
   return (
-    <Button colorScheme='blue'>Button</Button>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"yellow"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"orange"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"green.300"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
