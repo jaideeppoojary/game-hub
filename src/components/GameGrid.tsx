6fc3ba2f56589329be85ac306a0d7cfd7cffb6da
Jaideep <jaideep.jaidee@gmail.com>
2023-07-13 20:36:31 +0530

    Clean up the game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,9 +15,9 @@ const GameGrid = ({ gameQuery}: Props) => {
   
   return (
     <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
       padding={"25px"}
-      spacing={3}
+      spacing={6}
     >
       {isLoading &&
         Array.from(Array(10)).map((d, i) => (
