914260a76447d992e57ce2a4a112f701dea3db0e
Jaideep <jaideep.jaidee@gmail.com>
2023-08-01 00:10:20 +0530

    Added stale time for queryies

--- src/hooks/useScreenshots.ts
+++ src/hooks/useScreenshots.ts
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import APIClinet from '../services/api-client';
 import Screenshots from '../entities/Screenshots';
+import ms from 'ms';
 
 const useScreenshots = (game_pk: number) => {
   const apiClient = new APIClinet<Screenshots>(`/games/${game_pk}/screenshots`);
   return useQuery({
     queryKey: ['game_screenshots', game_pk],
-    queryFn: apiClient.getAll
+    queryFn: apiClient.getAll,
+    staleTime: ms('10m'),
   });
 };
 
--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import APIClinet from '../services/api-client';
 import Trailer from '../entities/Trailer';
+import ms from 'ms';
 
 const useTrailer = (gameId: number) => {
   const apiClient = new APIClinet<Trailer>(`/games/${gameId}/movies`);
   return useQuery({
     queryKey: ['trailer', gameId],
-    queryFn: apiClient.getAll
+    queryFn: apiClient.getAll,
+    staleTime: ms('20m'),
   });
 };
 
